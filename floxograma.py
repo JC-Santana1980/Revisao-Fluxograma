d2ad184e94c10f1572c464c276dd754705e638a9
JC-Santana1980 <santana.juliocesar@gmail.com>
2026-07-02 20:13:44 -0300

    Primeiro commit

---
+++ floxograma.py
@@ -0,0 +1,38 @@
+#Questao 01
+#N = float(input("Digite um numero"))
+#N = N * 2
+#print("O Ressultado é:", N) 
+
+#Questao 02
+#Idade = float(input("Digite a idade: "))
+#if Idade >= 18:
+# print("Maior de idade")
+#else:
+# print("Menor de idade")      
+
+
+#Questao 03
+# Nota1 = float(input("Digite a nota 1 : "))
+# Nota2 = float(input("Digite a nota 2 : "))
+# media = (Nota1 + Nota2) / 2
+# print("A media é:", media)
+
+#Questao 04
+# Numero = 1
+# while Numero <= 10 :
+#     print(" O numero atual é: ", Numero)    
+#     Numero = Numero + 1 
+
+#Questao 05
+# N1 = float(input("Digite um numero : "))
+# N2 = float(input("Digite outro numero : "))
+# soma = N1 + N2
+# subtracao = N1 - N2
+# multplicao = N1 * N2
+# divisao = N1 / N2
+# print("A soma dos numeros é: ", soma)
+# print("A subtracao dos numeros é: ", subtracao)
+# print("A multiplicao dos numeros é: ", multplicao)
+# print("A divisao dos numeros é: ", divisao)
+
+#Questao 06
